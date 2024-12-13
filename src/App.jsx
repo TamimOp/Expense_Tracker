ea629d499d066b2fdb10da27d22a7fc3ba85a462
TamimOp <tamimshadanik@gmail.com>
2024-12-13 21:14:56 +0600

    adding navbar and submitform components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Navbar from "./components/Navbar";
+import SubmitionForm from "./components/SubmitionForm";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="relative mx-auto mt-10 w-full max-w-7xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <SubmitionForm />
+        </div>
+      </div>
+    </>
+  );
 }
